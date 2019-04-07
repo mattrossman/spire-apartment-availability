0ed25a258d1c842fb16beb6d74a30d1b8cb44eaf
mattrossman <mrossman@umass.edu>
2019-04-08 01:04:59 +0200

    add optional form element for webhook after search
    A maximum 100 character formatted result summary will be delivered to
    the provided hook URL in a 'text' argument. Used to deliver results to a
    Zapier task for SMS delivery.

--- api.py
+++ api.py
@@ -1,11 +1,25 @@
 from flask import Flask, request, jsonify
 import spire
+from typing import List
+import requests
+
 
 app = Flask(__name__)
+
 
 @app.route('/search_area', methods=['POST'])
 def search_area():
     creds = request.form
     query = request.args
     s = spire.Session(creds['user'], creds['password'], creds['spire_id'])
-    return jsonify(s.search_area(query.get('area'), query.get('room_type')))
+    result = s.search_area(query.get('area'), query.get('room_type'))
+    if 'hook' in creds:
+        call_hook(result, creds['hook'])
+    return jsonify(result)
+
+
+def call_hook(rooms: List[dict], hook: str):
+    formatted = ['%s %s' % (room['building'].capitalize(), room['number']) for room in rooms]
+    out = ', '.join(formatted)
+    out = out[:97] + '...' if len(out) > 100 else out
+    requests.post(hook, data={'text': out})
