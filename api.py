998660eedcd30b5eb1a862d4f85453a9533fcb1d
mattrossman <mrossman@umass.edu>
2019-04-08 01:19:01 +0200

    remove summary truncation
    Using email delivery instead so no need to truncate.

--- api.py
+++ api.py
@@ -14,12 +14,12 @@ def search_area():
     s = spire.Session(creds['user'], creds['password'], creds['spire_id'])
     result = s.search_area(query.get('area'), query.get('room_type'))
     if 'hook' in creds:
-        call_hook(result, creds['hook'])
+        return call_hook(result, creds['hook'])
     return jsonify(result)
 
 
 def call_hook(rooms: List[dict], hook: str):
     formatted = ['%s %s' % (room['building'].capitalize(), room['number']) for room in rooms]
     out = ', '.join(formatted)
-    out = out[:97] + '...' if len(out) > 100 else out
     requests.post(hook, data={'text': out})
+    return out
